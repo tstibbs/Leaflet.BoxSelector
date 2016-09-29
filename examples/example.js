ac5becdbe0c8a616bec89bbd54340ea1e01875dd
tstibbs <tstibbs@users.noreply.github.com>
2016-09-29 20:33:09 +0100

    Starting from the box zoom handler

---
+++ examples/example.js
@@ -0,0 +1,23 @@
+var group = new L.LayerGroup();
+
+var markers = [
+	[51.503984, -0.118253],
+	[51.497466, -0.124905],
+	[51.506061, -0.116966],
+	[51.499102, -0.124648],
+	[51.503797, -0.143649],
+	[51.530535, -0.153362],
+	[51.500784, -0.143052],
+	[51.497770, -0.101477]
+];
+
+markers.forEach(function(element) {
+	L.marker(element).addTo(group);
+});
+
+var map = L.map('map', {
+	center: [51.515, -0.13],
+	zoom: 13,
+	layers: [group],
+	boxZoom: false
+});
